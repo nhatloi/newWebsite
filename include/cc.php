713b1237210c9f103d8baa22c04f2a5ce0793711
nhatloi <75830060+nhatloi@users.noreply.github.com>
2021-03-02 16:10:30 +0700

    Update cc.php

--- include/cc.php
+++ include/cc.php
@@ -6,8 +6,8 @@ define('DB_PASS' ,'1q2w3e4r');
 define('DB_NAME', 'db_a706a4_traicay');
 echo "Hello world!<br>";
 
-$ung=mysql_connect('mysql5039.site4now.net','a706a4_traicay','1q2w3e4r') ;
+$ung=mysqli_connect('mysql5039.site4now.net','a706a4_traicay','1q2w3e4r') ;
 echo "I'm about to learn PHP!<br>";
 $strSQL=mysqli_select_db('db_a706a4_traicay',$ung);
-mysql_query("SET NAMES 'utf8'");
+mysqli_query("SET NAMES 'utf8'");
 ?>
