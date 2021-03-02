ec5bbb2cf6aea23f28842c5e27e8a008127ae888
nhatloi <75830060+nhatloi@users.noreply.github.com>
2021-03-02 15:56:39 +0700

    Update cc.php

--- include/cc.php
+++ include/cc.php
@@ -1,9 +1,1 @@
-<?php
-define('DB_SERVER','mysql5039.site4now.net');
-define('DB_USER','a706a4_traicay');
-define('DB_PASS' ,'1q2w3e4r');
-define('DB_NAME', 'db_a706a4_traicay');
-$ung=mysql_connect('mysql5039.site4now.net','a706a4_traicay','1q2w3e4r') or die('Khong The Ket Noi Voi May Chu');
-$strSQL=mysql_select_db('db_a706a4_traicay',$ung);
-mysql_query("SET NAMES 'utf8'");
-?>
+
