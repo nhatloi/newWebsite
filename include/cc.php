4f794cf15cb47c3c02f7e22af12161fadfa4c00b
nhatloi <75830060+nhatloi@users.noreply.github.com>
2021-03-02 16:08:43 +0700

    Update cc.php

--- include/cc.php
+++ include/cc.php
@@ -8,6 +8,6 @@ echo "Hello world!<br>";
 
 $ung=mysql_connect('mysql5039.site4now.net','a706a4_traicay','1q2w3e4r') ;
 echo "I'm about to learn PHP!<br>";
-$strSQL=mysql_select_db('db_a706a4_traicay',$ung);
+$strSQL=mysqli_select_db('db_a706a4_traicay',$ung);
 mysql_query("SET NAMES 'utf8'");
 ?>
