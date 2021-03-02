da2ce4896a0e6747c3c34b7a7f2ad0bb02e5bc06
nhatloi <75830060+nhatloi@users.noreply.github.com>
2021-03-02 15:53:01 +0700

    Update ketnoi.php

--- include/ketnoi.php
+++ include/ketnoi.php
@@ -1,6 +1,4 @@
 <?php
 
-$ung=mysql_connect('mysql5039.site4now.net','a706a4_traicay','1q2w3e4r') or die('Khong The Ket Noi Voi May Chu');
-$strSQL=mysql_select_db('db_a706a4_traicay',$ung);
-mysql_query("SET NAMES 'utf8'");
+
 ?>
